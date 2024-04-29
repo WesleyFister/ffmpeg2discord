a1e49a52824414ef45fee2215e7f3203416d9690
Wesley Fister <fisterwesley@gmail.com>
2024-04-28 18:54:26 -0700

    Update FFmpeg2Discord.py

--- FFmpeg2Discord.py
+++ FFmpeg2Discord.py
@@ -1,79 +1,185 @@
-#!/usr/bin/env python3
-from os import getenv, path, remove
-from platform import system
-from mimetypes import guess_type
+from ffmpeg_progress_yield import FfmpegProgress
+from PyQt5 import QtCore, QtGui, QtWidgets
 from moviepy.editor import VideoFileClip
-from PyQt5.QtWidgets import QApplication, QFileDialog
-#TODO Keep original video rotation and get codec information.
+from ui import Ui_MainWindow
+from platform import system
+import subprocess
+import sys
+import os
 
-targetFileSize = 200000000
-audioBitrate = 60000
 
-filePaths = getenv("NAUTILUS_SCRIPT_SELECTED_FILE_PATHS")
 
-if system() == "Windows":
-	trash = "NUL"
-	
-elif system() == "Linux":
-	trash = "/dev/null"
-
-if filePaths == None:
-	# File selection dialogue if not invoked through Nautilus file manager.
-	app = QApplication([])
-	file_dialog = QFileDialog()
-	file_dialog.setFileMode(QFileDialog.ExistingFiles)
-	file_dialog.setViewMode(QFileDialog.Detail)
-	file_dialog.exec_()
-	filePathList = file_dialog.selectedFiles()
-	app.quit()
-	
-else:
-	# If invoked through Nautilus file manager.
-	filePathList = filePaths.split("\n")
-
-for filePath in filePathList:
-	mime = guess_type(filePath)
-	fileType = mime[0].split("/")
-	
-	fileSize = path.getsize(filePath)
-	
-	if fileType[0] == "video":
-	# If statement does not work, just proof of concept.
-	# if fileSize > 25000000 or videoCodec != "h264":
-		dirName, file = path.split(filePath)
-		fileName, fileExtension = path.splitext(file)
-		dirName = dirName + '/'
-		output_file = dirName + fileName + "Discord" + ".mp4"
+class ffmpeg2discord(Ui_MainWindow):	
+	def __init__(self, window):
+		self.filePathList = ""
+		self.ffmpegMode = "slow"
+		self.mergeAudio = ""
+		self.startTime = "" 
+		self.endTime = ""
+		self.targetFileSize = 200000000
+		self.audioBitrate = 60000
 		
-		# Calculates bitrate based on target file size.
-		videoLength = VideoFileClip(filePath).duration
-		bitrate = (targetFileSize/videoLength)-audioBitrate
-		bitrate = int(bitrate)
+		self.setupUi(window)
+		self.label.setText("0/0")
+		self.startTime = self.lineEdit.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("([0-5][0-9]):([0-5][0-9]):([0-5][0-9]).([0-9][0-9])")))
+		self.endTime = self.lineEdit_2.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("([0-5][0-9]):([0-5][0-9]):([0-5][0-9]).([0-9][0-9])")))
+		self.pushButton.clicked.connect(self.fileOpen)
+		self.radioButton.clicked.connect(lambda: self.radioOptions("fastest"))
+		self.radioButton_2.clicked.connect(lambda: self.radioOptions("slow"))
+		self.radioButton_3.clicked.connect(lambda: self.radioOptions("slowest"))
+		self.checkBox.stateChanged.connect(self.checkboxToggled)
+		self.buttonBox.rejected.connect(self.cancel)
+		self.buttonBox.accepted.connect(self.confirm)
+	
+	def convertTimeToSeconds(self, timeStr):
+		timeStr = timeStr.split(":")
+		while len(timeStr) < 3:
+			timeStr.insert(0, 0)
+			
+		totalSeconds = int(timeStr[0]) * 3600 + int(timeStr[1]) * 60 + int(timeStr[2])
+
+		return totalSeconds
+	
+	def fileOpen(self):
+		file_dialog = QtWidgets.QFileDialog()
+		file_dialog.setFileMode(QtWidgets.QFileDialog.ExistingFiles)
+		file_dialog.setViewMode(QtWidgets.QFileDialog.Detail)
+		file_dialog.exec_()
+		self.filePathList = file_dialog.selectedFiles()
+	
+	def radioOptions(self, optionType):
+		if optionType == "fastest":
+			self.ffmpegMode = "fastest"
+		elif optionType == "slow":
+			self.ffmpegMode = "slow"
+		elif optionType == "slowest":
+			self.ffmpegMode = "slowest"
 		
-		# Will only set FPS to 30 if video is greater than 50MB.
-		videoFPS = VideoFileClip(filePath).fps
-		if videoFPS > 30 and fileSize > 50000000:
-			videoFPS = 30
+	def checkboxToggled(self):
+		if self.checkBox.isChecked():
+			self.mergeAudio = "mergeAudio"
 		
-		# Will only set resolution to 960 and auto calculate width or height if video is greater than 100MB.
-		# Discord can embed VP9 but it will not work on ios.
-		clip = VideoFileClip(filePath)
-		width = clip.size[0]
-		height = clip.size[1]
-		resized_clip = clip.resize(1)
-		if (width > 720 or height > 720) and fileSize > 100000000:
-			if width > height:
-				resized_clip = clip.resize(width=720)
+		else:
+			self.mergeAudio = "noMergeAudio"
+	
+	def cancel(self):
+		app.exit()
+		
+	def confirm(self):
+		videoProgress = 0
+		
+		if system() == "Windows":
+			trash = "NUL"
+			
+		elif system() == "Linux":
+			trash = "/dev/null"
+		
+		for filePath in self.filePathList:
+			if filePath:
+				dirName, file = os.path.split(filePath)
+				fileName, fileExtension = os.path.splitext(file)
+				dirName = dirName + '/'
+				outputFile = dirName + fileName + "Discord" + ".mp4"
+				ffmpegCommand = ["./tools/ffmpeg", "-i", str(filePath)]
+				numOfVideos = len(self.filePathList)
 				
-			else:
-				resized_clip = clip.resize(height=720)
-		
-		# Write to file using 2 pass encoding and other FFmpeg options.
-		ffmpeg_params = ["-pass", "1", "-r", str(videoFPS), "-strict", "-2", "-c:v", "libx264", "-c:a", "libopus", "-b:v", str(bitrate), "-b:a", str(audioBitrate), "-preset", "veryslow", "-f", "mp4", trash]
-		resized_clip.write_videofile(output_file, ffmpeg_params=ffmpeg_params, verbose=False)
-		ffmpeg_params = ["-pass", "2", "-r", str(videoFPS), "-strict", "-2", "-c:v", "libx264", "-c:a", "libopus", "-b:v", str(bitrate), "-b:a", str(audioBitrate), "-preset", "veryslow", "-f", "mp4"]
-		resized_clip.write_videofile(output_file, ffmpeg_params=ffmpeg_params, verbose=False)
-		
-		# Remove files created by FFmpeg in the first pass.
-		remove("ffmpeg2pass-0.log")
-		remove("ffmpeg2pass-0.log.mbtree")
+				self.startTime = self.lineEdit.text()
+				self.endTime = self.lineEdit_2.text()
+				print(self.startTime)
+				
+				# Determine bitrate based on length of video.
+				videoLength = VideoFileClip(filePath).duration
+				if self.startTime != "" and self.endTime == "":
+					videoLength = videoLength - self.convertTimeToSeconds(self.startTime)
+					ffmpegCommand.extend(["-ss", self.startTime])
+					
+				elif self.startTime == "" and self.endTime != "":
+					videoLength = self.convertTimeToSeconds(self.endTime)
+					ffmpegCommand.extend(["-to", self.endTime])
+				
+				elif self.startTime != "" and self.endTime != "":
+					videoLength = self.convertTimeToSeconds(self.endTime) - self.convertTimeToSeconds(self.startTime)
+					ffmpegCommand.extend(["-ss", self.startTime, "-to", self.endTime])
+				
+				bitrate = (self.targetFileSize/videoLength)-self.audioBitrate
+				bitrate = int(bitrate)
+				
+				fileSize = os.path.getsize(filePath)
+				
+				videoFPS = VideoFileClip(filePath).fps
+				if videoFPS > 30 and fileSize > 50000000:
+					ffmpegCommand.extend(["-r", "30"])
+				
+				clip = VideoFileClip(filePath)
+				width = clip.size[0]
+				height = clip.size[1]
+				if (width > 720 or height > 720) and fileSize > 100000000:
+					if width > height:
+						ffmpegCommand.extend(["-vf", "scale='trunc(oh*a/2)*2:720'"])
+						
+					else:
+						ffmpegCommand.extend(["-vf", "scale='720:trunc(oh*a/2)*2'"])
+
+
+					
+				if self.mergeAudio == "mergeAudio":
+					numAudioStreams = subprocess.run(["./tools/ffprobe", "-loglevel", "error", "-select_streams", "a", "-show_entries", "stream=codec_type", "-of", "csv=p=0", filePath], capture_output=True, text=True)
+					numAudioStreams = len(numAudioStreams.stdout.strip().splitlines())
+					ffmpegCommand.extend(["-filter_complex", "[0:a]amerge=inputs={}[a]".format(numAudioStreams), '-map', '[a]'])
+					
+				else:
+					ffmpegCommand.extend(["-map", "0:a:0"])
+				
+				# FFmpeg commands based on user selected options.
+				if self.ffmpegMode == "fastest":
+					ffmpegCommand.extend(["-preset", "ultrafast", "-c:v", "libx264"])
+					ffmpegCommand2 = ffmpegCommand.copy()
+					
+					ffmpegCommand.extend(["-pass", "1"])
+					ffmpegCommand2.extend(["-pass", "2"])
+					
+					progressPercent = 2
+					progressPercent2 = 2
+					progressPercent3 = 50
+					
+				elif self.ffmpegMode == "slow":
+					ffmpegCommand.extend(["-preset", "veryslow", "-c:v", "libx264"])
+					ffmpegCommand2 = ffmpegCommand.copy()
+					
+					ffmpegCommand.extend(["-pass", "1"])
+					ffmpegCommand2.extend(["-pass", "2"])
+					
+					progressPercent = 5
+					progressPercent2 = (5/4)
+					progressPercent3 = 20
+					
+				elif self.ffmpegMode == "slowest":
+					ffmpegCommand.extend(["-deadline", "best", "-c:v", "libvpx-vp9", "-row-mt", "1"])
+					ffmpegCommand2 = ffmpegCommand.copy()
+					
+					progressPercent = 2
+					progressPercent2 = 2
+					progressPercent3 = 50
+					
+					
+				videoProgress += 1
+				self.label.setText(str(videoProgress) + "/" + str(numOfVideos))
+				ffmpegCommand.extend(["-ac", "2", "-map", "0:v", "-b:v", str(bitrate), "-b:a", str(self.audioBitrate), "-c:a", "libopus", "-f", "mp4", str(trash), "-y"])
+				print(ffmpegCommand)
+				ff = FfmpegProgress(ffmpegCommand)
+				for progress in ff.run_command_with_progress():
+					self.progressBar.setValue(int(progress / progressPercent))
+					print(progress)
+				
+				ffmpegCommand2.extend(["-ac", "2", "-map", "0:v", "-b:v", str(bitrate), "-b:a", str(self.audioBitrate), "-c:a", "libopus", outputFile, "-y"])
+				ff = FfmpegProgress(ffmpegCommand2)
+				for progress in ff.run_command_with_progress():
+					self.progressBar.setValue(int(progress / progressPercent2) + progressPercent3)
+					print(progress)
+				
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	MainWindow = QtWidgets.QMainWindow()
+	ui = ffmpeg2discord(MainWindow)
+	MainWindow.show()
+	sys.exit(app.exec_())
